5350b215c73ff9f1da97e5e2487fa08341ee7f8e
Lucas Newlands <lucasnewlands@MacBook-Pro-de-Lucas.local>
2023-04-27 16:49:02 -0300

    feat: Tweet interface

--- TwitterClone/Controllers/Core/HomeViewController.swift
+++ TwitterClone/Controllers/Core/HomeViewController.swift
@@ -9,10 +9,39 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    private let timeLineTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(TweetTableTableViewCell.self, forCellReuseIdentifier: TweetTableTableViewCell.identifier)
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(timeLineTableView)
+        timeLineTableView.delegate = self
+        timeLineTableView.dataSource = self
         
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        timeLineTableView.frame = view.frame
+    }
+    
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TweetTableTableViewCell.identifier, for: indexPath) as? TweetTableTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        return cell
     }
     
 }
