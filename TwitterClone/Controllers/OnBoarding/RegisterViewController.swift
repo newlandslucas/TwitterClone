7895acca92d4f2e9f607451f5060b0df8e54c7fe
Lucas Newlands <lucasnewlands@MacBook-Pro-de-Lucas.local>
2023-05-03 02:16:49 -0300

    feat: Validation on Form RegisterViewController

--- TwitterClone/Controllers/OnBoarding/RegisterViewController.swift
+++ TwitterClone/Controllers/OnBoarding/RegisterViewController.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import Combine
 
 class RegisterViewController: UIViewController {
+    
+    private var viewModel = RegisterViewModel()
+    private var subsscriptions: Set<AnyCancellable> = []
     
     private let registerTitleLabel: UILabel = {
        let label = UILabel()
@@ -48,8 +52,32 @@ class RegisterViewController: UIViewController {
         button.backgroundColor = UIColor(red: 29/255, green: 161/255, blue: 242/255, alpha: 1)
         button.layer.masksToBounds = true
         button.layer.cornerRadius = 25
+        button.isEnabled = false
         return button
     }()
+    
+    @objc private func didChangeEmailField() {
+        viewModel.email = emailTextField.text
+        viewModel.validateRegistrationForm()
+    }
+    
+    @objc private func didChangePasswordField() {
+        viewModel.password = passwordTextField.text
+        viewModel.validateRegistrationForm()
+    }
+    
+    private func bindViews() {
+        emailTextField.addTarget(self, action: #selector(didChangeEmailField), for: .editingChanged)
+        passwordTextField.addTarget(self, action: #selector(didChangePasswordField), for: .editingChanged)
+        viewModel.$isRegistrationFormValid.sink { [weak self] validationState in
+            self?.registerButton.isEnabled = validationState
+        }
+        .store(in: &subsscriptions)
+    }
+    
+    @objc private func didTapToDismiss() {
+        view.endEditing(true)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -59,6 +87,8 @@ class RegisterViewController: UIViewController {
         view.addSubview(passwordTextField)
         view.addSubview(registerButton)
         configureConstraints()
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTapToDismiss)))
+        bindViews()
         
     }
     
